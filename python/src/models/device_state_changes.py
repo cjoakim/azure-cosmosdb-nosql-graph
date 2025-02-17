b4f8b8aa910b24449d29d5bb416255e742696e74
Chris Joakim <christopher.joakim@gmail.com>
2025-02-17 08:39:37 -0500

    device_state_changes.py and its' unit test

--- python/src/models/device_state_changes.py
+++ python/src/models/device_state_changes.py
@@ -2,29 +2,68 @@ import time
 
 from src.models.device_data import DeviceData
 
+# Instances of this class are used to detect and identify changes,
+# if any, between one DeviceState object and other.
+# Chris Joakim, Microsoft
+
 class DeviceStateChanges:
 
+    cosmos_generated_attrs = "_rid,_self,_etag,_attachments,_ts".split(',')
+    
     def __init__(self, curr_doc, ds_doc):
         self.changes = 'none'
         self.curr_doc = curr_doc
         self.ds_doc = ds_doc
-        self.updated_doc = None
-        self.attr_changes = list()
+        self.updated_doc = dict()
+        self.attrs_added = list()
+        self.attrs_removed = list()
+        self.attrs_changed = list()
 
         if self.ds_doc is None:
             return  # bad input, the streamed ds_doc is expected to be non-null
-        
-        if self.curr_doc is None:
+    
+        if (self.curr_doc is None) or (len(self.curr_doc.keys()) == 0):
             self.changes = 'new_doc'
             self.updated_doc = self.ds_doc
             for attr in ds_doc.keys():
-                self.attr_changes.append(attr)
+                self.attrs_added.append(attr)
         else:
-            pass
+            self.compare_docs(self.curr_doc, self.ds_doc)
 
         if self.changes == 'none':
             self.updated_doc = None
 
     def has_changes(self):
         return self.changes != 'none'
+    
+    def compare_docs(self, curr_doc, ds_doc):
+        merged_keys = self.merge_keys(self.curr_doc, self.ds_doc)
+        for key in merged_keys:
+            if key in DeviceStateChanges.cosmos_generated_attrs:
+                # don't compare the cosmos system-generated attbibutes,
+                # but do add them to the updated doc for upsert operation
+                if key in curr_doc.keys():
+                    self.updated_doc[key] = curr_doc[key]
+            else:
+                if key not in ds_doc:
+                    self.changes = 'attr'
+                    self.attrs_removed.append(key)
+                elif key not in curr_doc:
+                    self.changes = 'attr'
+                    self.attrs_added.append(key)
+                    self.updated_doc[key] = ds_doc[key]
+                elif curr_doc[key] != ds_doc[key]:
+                    self.changes = 'attr'
+                    self.attrs_changed.append(key)
+                    self.updated_doc[key] = ds_doc[key]
+                else:
+                    self.updated_doc[key] = curr_doc[key]
 
+    def merge_keys(self, curr_doc, ds_doc):
+        merged = dict()
+        for key in curr_doc.keys():
+            merged[key] = 0
+        for key in ds_doc.keys():
+            merged[key] = 0
+        return sorted(merged.keys())
+    
--- python/tests/test_device_state_changes.py
+++ python/tests/test_device_state_changes.py
@@ -1,8 +1,5 @@
-import os
-import pytest
+import time
 
-from src.util.template import Template
-from src.services.config_service import ConfigService
 from src.models.device_data import DeviceData
 from src.models.device_state_changes import DeviceStateChanges
 
@@ -17,21 +14,17 @@ def test_new_device_state():
     assert dsc.has_changes() == True
     assert dsc.changes == 'new_doc'
     assert dsc.updated_doc != None
-    assert sorted(dsc.attr_changes) == sorted(ds_doc.keys())
+    assert sorted(dsc.attrs_added) == sorted(ds_doc.keys())
+    assert len(dsc.attrs_added) == len(ds_doc.keys())
+    assert len(dsc.attrs_removed) == 0
+    assert len(dsc.attrs_changed) == 0
 
     for attr_name in sorted(ds_doc.keys()):
         assert dsc.updated_doc[attr_name] == ds_doc[attr_name]
 
-def test_no_changes():
-    curr_doc = DeviceData.random_device_state()
-    ds_doc = dict(curr_doc)
-    dsc = DeviceStateChanges(curr_doc, ds_doc)
-    assert dsc.has_changes() == False
-    assert dsc.changes == 'none'
-    assert dsc.updated_doc == None
+    assert '_etag' not in dsc.updated_doc.keys()
 
-def test_strong_change():
-    # TODO - test and impl is WIP
+def test_no_changes():
     curr_doc = DeviceData.random_device_state()
     curr_doc['_etag'] = DeviceData.simulated_etag()
     ds_doc = dict(curr_doc)
@@ -39,3 +32,62 @@ def test_strong_change():
     assert dsc.has_changes() == False
     assert dsc.changes == 'none'
     assert dsc.updated_doc == None
+    assert len(dsc.attrs_added) == 0
+    assert len(dsc.attrs_removed) == 0
+    assert len(dsc.attrs_changed) == 0
+
+def test_attr_change():
+    curr_doc = DeviceData.random_device_state()
+    curr_doc['_etag'] = DeviceData.simulated_etag()
+    ds_doc = dict(curr_doc)
+    ds_doc['mac'] = int(time.time())
+    dsc = DeviceStateChanges(curr_doc, ds_doc)
+    print(dsc.updated_doc)
+    assert dsc.has_changes() == True
+    assert dsc.changes == 'attr'
+    assert dsc.updated_doc != None
+    assert len(curr_doc.keys()) == len(dsc.updated_doc.keys())
+    assert len(dsc.attrs_added) == 0
+    assert len(dsc.attrs_removed) == 0
+    assert len(dsc.attrs_changed) == 1
+    assert dsc.attrs_changed == ['mac']
+
+    assert curr_doc['_etag'] == dsc.updated_doc['_etag']
+
+def test_attr_added():
+    curr_doc = DeviceData.random_device_state()
+    curr_doc['_etag'] = DeviceData.simulated_etag()
+    ds_doc = dict(curr_doc)
+    ds_doc['cat'] = 'elsa'
+    dsc = DeviceStateChanges(curr_doc, ds_doc)
+    print(dsc.updated_doc)
+    assert dsc.has_changes() == True
+    assert dsc.changes == 'attr'
+    assert dsc.updated_doc != None
+    expected_attr_count = len(curr_doc.keys()) + 1
+    assert len(dsc.updated_doc.keys()) == expected_attr_count
+    assert len(dsc.attrs_added) == 1
+    assert len(dsc.attrs_removed) == 0
+    assert len(dsc.attrs_changed) == 0
+    assert dsc.attrs_added == ['cat']
+
+    assert curr_doc['_etag'] == dsc.updated_doc['_etag']
+
+def test_attr_removed():
+    curr_doc = DeviceData.random_device_state()
+    curr_doc['_etag'] = DeviceData.simulated_etag()
+    ds_doc = dict(curr_doc)
+    del ds_doc['build']
+    dsc = DeviceStateChanges(curr_doc, ds_doc)
+    print(dsc.updated_doc)
+    assert dsc.has_changes() == True
+    assert dsc.changes == 'attr'
+    assert dsc.updated_doc != None
+    expected_attr_count = len(curr_doc.keys()) - 1
+    assert len(dsc.updated_doc.keys()) == expected_attr_count
+    assert len(dsc.attrs_added) == 0
+    assert len(dsc.attrs_removed) == 1
+    assert len(dsc.attrs_changed) == 0
+    assert dsc.attrs_removed == ['build']
+
+    assert curr_doc['_etag'] == dsc.updated_doc['_etag']
